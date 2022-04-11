2255228ed5fdfc4c409f45444864615806ed3d3b
kyoko0522 <kyo08023971867@gmail.com>
2022-04-11 13:33:41 +0000

    Item update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,28 +11,38 @@ class ItemsController < ApplicationController
   end
 
   def index
-    @user.name = current_user.name
-    @items = current_user.item.all
+    # @user.name = current_user.name
+    @items = current_user.items.all
 
   end
+
+  def show
+     @item = Item.find(params[:id])
+  end
+
 
   def edit
     @item = Item.find(params[:id])
   end
 
   def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to items_path
   end
 
 
 
   def destroy
-
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to items_path
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :image, :notice, :price, :amount, :code)
+    params.require(:item).permit(:name, :image, :notice, :price, :amount, :code, :until)
   end
 
 
